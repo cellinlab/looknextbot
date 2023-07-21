6909ef2c6a04d0e7fbfa2da74466f40f37e12af7
cell <cellinlab@gmail.com>
2023-07-21 11:26:35 +0800

    feat: remove conversation

--- api/bot.js
+++ api/bot.js
@@ -1,6 +1,5 @@
 import { Bot, webhookCallback, session } from "grammy";
 import { Menu } from '@grammyjs/menu';
-import { conversations, createConversation } from '@grammyjs/conversations';
 import "dotenv/config";
 
 const token = process.env.BOT_TOKEN;
@@ -12,9 +11,7 @@ if (!token) {
 const bot = new Bot(token);
 
 const menu = new Menu('my-menu')
-  .text("🔼 Add", async (ctx) => {
-    await ctx.conversation.enter("add");
-  })
+  .text("🔼 Add", handleAdd)
   .text("🔀 Switch", (ctx) => {
     ctx.session.isBuy = !ctx.session.isBuy;
     ctx.menu.update();
@@ -54,70 +51,54 @@ bot.use(session({
   initial: initialSession,
 }));
 
-bot.use(conversations());
-bot.use(createConversation(greeting));
-bot.use(createConversation(handleAdd, "add"));
-
 bot.use(menu);
 
 bot.command("start", async (ctx) => {
-  await ctx.conversation.enter("greeting");
+  await ctx.reply("Hello, welcome to here, there is looknextbot", {
+    reply_markup: menu
+  });
 });
 
 bot.catch((err) => {
   console.log("Error: ", err);
 });
 
-async function greeting(conversation, ctx) {
-  await ctx.reply("Hello, welcome to here, there is looknextbot", {
-    reply_markup: menu
-  });
-
-  return;
-}
-
-async function handleAdd(conversation, ctx) {
+async function handleAdd(ctx) {
   try {
-    console.log('conversation', conversation);
-    console.log('ctx', ctx);
     await ctx.reply("Please enter the name you want to add（name must be letters and length 1-8）");
 
-    let resName = await conversation.wait();
+    console.log("ctx: ", ctx);
 
-    let name = resName.update.message.text;
+    // let name = resName.update.message.text;
 
-    while (!/^[a-zA-Z]{1,8}$/.test(name)) {
-      await ctx.reply("name must be letters and length 1-8");
+    // while (!/^[a-zA-Z]{1,8}$/.test(name)) {
+    //   await ctx.reply("name must be letters and length 1-8");
 
-      resName = await conversation.wait();
-      name = resName.update.message.text;
-    }
+    //   resName = await conversation.wait();
+    //   name = resName.update.message.text;
+    // }
 
     ctx.session.name = name;
 
-    // 地址必须是长度为42位，0x开头，由数字和字母组成的字符串
-    await ctx.reply("Please enter the address you want to add（address must be 42 characters long, start with 0x, and consist of numbers and letters）");
+    // // 地址必须是长度为42位，0x开头，由数字和字母组成的字符串
+    // await ctx.reply("Please enter the address you want to add（address must be 42 characters long, start with 0x, and consist of numbers and letters）");
 
-    let resAddress = await conversation.wait();
+    // let resAddress = await conversation.wait();
 
-    let address = resAddress.update.message.text;
+    // let address = resAddress.update.message.text;
 
-    while (!/^0x[a-zA-Z0-9]{40}$/.test(address)) {
-      await ctx.reply("address must be 42 characters long, start with 0x, and consist of numbers and letters");
+    // while (!/^0x[a-zA-Z0-9]{40}$/.test(address)) {
+    //   await ctx.reply("address must be 42 characters long, start with 0x, and consist of numbers and letters");
 
-      resAddress = await conversation.wait();
-      address = resAddress.update.message.text;
-    }
+    //   resAddress = await conversation.wait();
+    //   address = resAddress.update.message.text;
+    // }
 
     ctx.session.address = address;
 
+    await ctx.reply("Add success");
 
-    console.log('conversation.menu', conversation.menu);
-    console.log('ctx.menu', ctx.menu);
-
-    await conversation.menu.update();
-
-    return;
+    ctx.menu.update();
   } catch (error) {
     console.log("error: ", error);
     await ctx.reply("Add failed");
